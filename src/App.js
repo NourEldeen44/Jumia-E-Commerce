3989b336dfe0938b70d38086c70b184d7d73c099
NourEldeen <neldeen8@gmail.com>
2022-07-15 17:45:53 +0200

    adding components including header file , pages folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,106 @@
 import "./App.css";
+import Header from "./components/header/header";
 import Firetest from "./firetest/firetest";
 
 function App() {
-  return <Firetest />;
+  return (
+    <div>
+      <Header />
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error tempore
+        voluptates sequi magnam! Rem aperiam voluptatum beatae quod explicabo
+        architecto incidunt nulla! Assumenda quibusdam debitis expedita impedit,
+        distinctio veniam vel. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Tenetur non accusamus in sit, ratione obcaecati
+        ducimus ipsam veritatis corporis, maiores aut omnis eaque veniam natus
+        consectetur reiciendis doloribus illum molestias. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Nam animi minima adipisci ipsa
+        repellat corrupti pariatur voluptatibus natus, expedita, maxime fugit
+        ullam facere velit. Numquam voluptate cum quibusdam repudiandae
+        repellendus! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Error tempore voluptates sequi magnam! Rem aperiam voluptatum beatae
+        quod explicabo architecto incidunt nulla! Assumenda quibusdam debitis
+        expedita impedit, distinctio veniam vel. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Tenetur non accusamus in sit, ratione
+        obcaecati ducimus ipsam veritatis corporis, maiores aut omnis eaque
+        veniam natus consectetur reiciendis doloribus illum molestias. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Nam animi minima
+        adipisci ipsa repellat corrupti pariatur voluptatibus natus, expedita,
+        maxime fugit ullam facere velit. Numquam voluptate cum quibusdam
+        repudiandae repellendus! Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Error tempore voluptates sequi magnam! Rem aperiam
+        voluptatum beatae quod explicabo architecto incidunt nulla! Assumenda
+        quibusdam debitis expedita impedit, distinctio veniam vel. Lorem ipsum
+        dolor, sit amet consectetur adipisicing elit. Tenetur non accusamus in
+        sit, ratione obcaecati ducimus ipsam veritatis corporis, maiores aut
+        omnis eaque veniam natus consectetur reiciendis doloribus illum
+        molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+        animi minima adipisci ipsa repellat corrupti pariatur voluptatibus
+        natus, expedita, maxime fugit ullam facere velit. Numquam voluptate cum
+        quibusdam repudiandae repellendus! Lorem ipsum dolor sit, amet
+        consectetur adipisicing elit. Error tempore voluptates sequi magnam! Rem
+        aperiam voluptatum beatae quod explicabo architecto incidunt nulla!
+        Assumenda quibusdam debitis expedita impedit, distinctio veniam vel.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur non
+        accusamus in sit, ratione obcaecati ducimus ipsam veritatis corporis,
+        maiores aut omnis eaque veniam natus consectetur reiciendis doloribus
+        illum molestias. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nam animi minima adipisci ipsa repellat corrupti pariatur
+        voluptatibus natus, expedita, maxime fugit ullam facere velit. Numquam
+        voluptate cum quibusdam repudiandae repellendus! Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Error tempore voluptates sequi
+        magnam! Rem aperiam voluptatum beatae quod explicabo architecto incidunt
+        nulla! Assumenda quibusdam debitis expedita impedit, distinctio veniam
+        vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+        non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus!
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,364 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faBox,
+  faCircleQuestion,
+  faCoffee,
+  faComment,
+  faHeart,
+  faMagnifyingGlass,
+  faShoppingBasket,
+  faShoppingCart,
+  faUser,
+  faUserAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import jumialogo from "../../images/Jumia-Logo.png";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div style={{ marginBottom: "90px" }}>
+      <nav
+        class="navbar navbar-expand-lg  bg-white d-flex justify-content-center mb-3"
+        style={{
+          height: "75px",
+          position: "fixed",
+          backgroundColor: "white",
+          width: "100vw",
+        }}
+      >
+        <div
+          class="container-fluid nav-container jumia-container"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <a class="navbar-brand" href="#">
+            <img
+              src={jumialogo}
+              alt="jumia Svg"
+              height={"90px"}
+              className="jumialogo"
+            />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+
+          <form class="d-flex search " role="search">
+            <div class="d-flex flex-row border border-1 border-Secondary flex-grow-1 mx-2">
+              <input
+                class="search-input m-2 border-0 flex-grow-1"
+                type="text"
+                placeholder="Search products, brands and categories"
+                aria-label="Search"
+                style={{
+                  width: "32vw",
+                  borderColor: "#c7c7cd",
+                  borderRadius: "8px",
+                }}
+              />
+              <div class="my-auto mx-2">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  size="lg"
+                  color="grey"
+                />
+              </div>
+            </div>
+            <button
+              class="btn search-button text-white"
+              type="submit"
+              style={{
+                fontWeight: "600",
+                boxShadow: "0 4px 8px 0 rgb(0 0 0 / 20%)",
+              }}
+            >
+              Search
+            </button>
+          </form>
+          <div
+            class="collapse navbar-collapse flex-grow-0"
+            // id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link nav-main dropdown-toggle text-dark"
+                  href="#"
+                  id="AccountDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faUser} color="black" size="2x" />
+                  <span style={{ marginLeft: "15px" }}>Account</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="AccountDropdown">
+                  <li class="d-flex align-items-center mt-2">
+                    <button className="sign btn search-button  text-white mx-auto w-75">
+                      Sign In
+                    </button>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <FontAwesomeIcon icon={faUser} color="black" />
+                      <span> My Account</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <FontAwesomeIcon icon={faBox} color="black" />
+                      <span> Orders</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-regular fa-heart"></i>
+                      <FontAwesomeIcon icon={faHeart} color="red" />
+                      <span> Saved Items</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown ">
+                <a
+                  class="nav-link nav-main dropdown-toggle text-dark"
+                  href="#"
+                  id="HelpDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon
+                    icon={faCircleQuestion}
+                    color="black"
+                    size="2x"
+                  />
+                  <span style={{ marginLeft: "15px" }}>Help</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="HelpDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Help Center
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Place & Track Order
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Order Cancellation
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Returns & Refounds
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Paymant & Jumia Account
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li class="d-flex align-items-center mb-2">
+                    <a class="btn search-button  text-white mx-auto w-75">
+                      <FontAwesomeIcon
+                        icon={faComment}
+                        color="white"
+                        size="lg"
+                      />
+                      <span style={{ marginLeft: "15px" }}>LiveChat</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link nav-main active text-dark"
+                  aria-current="page"
+                  href="#"
+                  style={{
+                    display: "flex",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faShoppingCart}
+                    size="2x"
+                    color="black"
+                  />
+                  <span
+                    style={{
+                      marginLeft: "15px",
+                      display: "flex",
+                      flexWrap: "nowrap",
+                    }}
+                  >
+                    Cart
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* drop down menu */}
+          <div
+            class="collapse navbar-collapse-sm d-lg-none dropmenu"
+            id="navbarSupportedContent"
+          >
+            <ul
+              class=""
+              style={{
+                display: "flex",
+                listStyle: "none",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100vw",
+                transitionTimingFunction: "ease-out",
+              }}
+            >
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle text-dark"
+                  href="#"
+                  id="AccountDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faUser} color="white" size="2x" />
+                  <span style={{ marginLeft: "15px" }}>Account</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="AccountDropdown">
+                  <li class="d-flex align-items-center mt-2">
+                    <button className="sign btn search-button  text-white mx-auto w-75">
+                      Sign In
+                    </button>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <FontAwesomeIcon icon={faUser} color="black" />
+                      <span> My Account</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <FontAwesomeIcon icon={faBox} color="black" />
+                      <span> Orders</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-regular fa-heart"></i>
+                      <FontAwesomeIcon icon={faHeart} color="red" />
+                      <span> Saved Items</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown ">
+                <a
+                  class="nav-link dropdown-toggle text-dark"
+                  href="#"
+                  id="HelpDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon
+                    icon={faCircleQuestion}
+                    color="white"
+                    size="2x"
+                  />
+                  <span style={{ marginLeft: "15px" }}>Help</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="HelpDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Help Center
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Place & Track Order
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Order Cancellation
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Returns & Refounds
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Paymant & Jumia Account
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li class="d-flex align-items-center mb-2">
+                    <a class="btn search-button  text-white mx-auto w-75">
+                      <FontAwesomeIcon
+                        icon={faComment}
+                        color="white"
+                        size="lg"
+                      />
+                      <span>LiveChat</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active text-dark"
+                  aria-current="page"
+                  href="#"
+                  style={{
+                    display: "flex",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faShoppingCart}
+                    size="2x"
+                    color="white"
+                  />
+                  <span style={{ marginLeft: "15px" }}>Cart</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
