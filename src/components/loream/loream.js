366dd9b95a2d08fc23b1b4621c6cb382a5445b40
NourEldeen <neldeen8@gmail.com>
2022-07-16 20:28:02 +0200

    adding language context & editing style

---
+++ src/components/loream/loream.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+const Loream = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error tempore
+        voluptates sequi magnam! Rem aperiam voluptatum beatae quod explicabo
+        architecto incidunt nulla! Assumenda quibusdam debitis expedita impedit,
+        distinctio veniam vel. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Tenetur non accusamus in sit, ratione obcaecati
+        ducimus ipsam veritatis corporis, maiores aut omnis eaque veniam natus
+        consectetur reiciendis doloribus illum molestias. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Nam animi minima adipisci ipsa
+        repellat corrupti pariatur voluptatibus natus, expedita, maxime fugit
+        ullam facere velit. Numquam voluptate cum quibusdam repudiandae
+        repellendus! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Error tempore voluptates sequi magnam! Rem aperiam voluptatum beatae
+        quod explicabo architecto incidunt nulla! Assumenda quibusdam debitis
+        expedita impedit, distinctio veniam vel. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Tenetur non accusamus in sit, ratione
+        obcaecati ducimus ipsam veritatis corporis, maiores aut omnis eaque
+        veniam natus consectetur reiciendis doloribus illum molestias. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Nam animi minima
+        adipisci ipsa repellat corrupti pariatur voluptatibus natus, expedita,
+        maxime fugit ullam facere velit. Numquam voluptate cum quibusdam
+        repudiandae repellendus! Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Error tempore voluptates sequi magnam! Rem aperiam
+        voluptatum beatae quod explicabo architecto incidunt nulla! Assumenda
+        quibusdam debitis expedita impedit, distinctio veniam vel. Lorem ipsum
+        dolor, sit amet consectetur adipisicing elit. Tenetur non accusamus in
+        sit, ratione obcaecati ducimus ipsam veritatis corporis, maiores aut
+        omnis eaque veniam natus consectetur reiciendis doloribus illum
+        molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+        animi minima adipisci ipsa repellat corrupti pariatur voluptatibus
+        natus, expedita, maxime fugit ullam facere velit. Numquam voluptate cum
+        quibusdam repudiandae repellendus! Lorem ipsum dolor sit, amet
+        consectetur adipisicing elit. Error tempore voluptates sequi magnam! Rem
+        aperiam voluptatum beatae quod explicabo architecto incidunt nulla!
+        Assumenda quibusdam debitis expedita impedit, distinctio veniam vel.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur non
+        accusamus in sit, ratione obcaecati ducimus ipsam veritatis corporis,
+        maiores aut omnis eaque veniam natus consectetur reiciendis doloribus
+        illum molestias. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nam animi minima adipisci ipsa repellat corrupti pariatur
+        voluptatibus natus, expedita, maxime fugit ullam facere velit. Numquam
+        voluptate cum quibusdam repudiandae repellendus! Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Error tempore voluptates sequi
+        magnam! Rem aperiam voluptatum beatae quod explicabo architecto incidunt
+        nulla! Assumenda quibusdam debitis expedita impedit, distinctio veniam
+        vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+        non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Error tempore voluptates
+        sequi magnam! Rem aperiam voluptatum beatae quod explicabo architecto
+        incidunt nulla! Assumenda quibusdam debitis expedita impedit, distinctio
+        veniam vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Tenetur non accusamus in sit, ratione obcaecati ducimus ipsam veritatis
+        corporis, maiores aut omnis eaque veniam natus consectetur reiciendis
+        doloribus illum molestias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nam animi minima adipisci ipsa repellat corrupti
+        pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
+        Numquam voluptate cum quibusdam repudiandae repellendus!
+      </div>
+    </div>
+  );
+};
+
+export default Loream;
