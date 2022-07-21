e771a267803f24c7f93964bab2a69c11122ea3aa
NourEldeen <neldeen8@gmail.com>
2022-07-21 12:39:32 +0200

    adding snap scroll component

--- src/components/loream/loream.js
+++ src/components/loream/loream.js
@@ -1,8 +1,39 @@
 import React from "react";
-
+import { useInViewport } from "react-in-viewport";
+import { useRef } from "react";
+import "./loream.css";
+import Snapscroll from "../snapscroll/snapscroll";
 const Loream = () => {
+  const divRef = useRef(null);
+  const ref = useRef();
+  const isInPortView = useInViewport(divRef);
+  // useScrollSnap({
+  //   ref: divRef,
+  //   duration: 100,
+  //   delay: 50,
+  // });
+  // const el = document.getElementById("snap").style;
+  const scroll = () => {
+    console.log();
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const imagesData = [
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+    "https://eg.jumia.is/cms/23-22/Thumbnails/AR/Flash_sales_AR.png",
+  ];
+  console.log(isInPortView);
   return (
-    <div>
+    <div ref={ref} id="sn">
       <div>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error tempore
         voluptates sequi magnam! Rem aperiam voluptatum beatae quod explicabo
@@ -96,6 +127,42 @@ const Loream = () => {
         pariatur voluptatibus natus, expedita, maxime fugit ullam facere velit.
         Numquam voluptate cum quibusdam repudiandae repellendus!
       </div>
+      {/* <button onClick={scroll}>click ti scroll</button>
+      <div
+        className="snap"
+        id="snap"
+        style={{
+          width: "500px",
+          overflowX: "auto",
+          overflowY: "hidden",
+          whiteSpace: "nowrap",
+          scrollSnapType: "x mandatory",
+        }}
+        // ref={divRef}
+      >
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+        <div style={{ width: "200px", margin: "20px" }} ref={divRef}>
+          {isInPortView ? "ELEMENT IS IN PORT VIEW" : "NOT IN PORTVIEW"}
+        </div>
+      </div> */}
+      <Snapscroll images={imagesData}></Snapscroll>
     </div>
   );
 };
---
+++ src/components/snapscroll/snapscroll.js
@@ -0,0 +1,102 @@
+/* eslint-disable eqeqeq */
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import "./snapscroll.css";
+
+const Snapscroll = (props) => {
+  const [images, setimages] = useState([]);
+  useEffect(() => {
+    setimages(props.images);
+  }, []);
+  const [snapScrollScreen, setsnapScrollScreen] = useState("start");
+  const [nxtBtnDisabled, setnxtBtnDisabled] = useState(false);
+  const [prvBtnDisabled, setprvBtnDisabled] = useState(true);
+  const firstImageRef = useRef(null);
+  const lastImageRef = useRef(null);
+  const nxtBtnRef = useRef(null);
+  const prvBtnRef = useRef(null);
+  const scrollEnd = () => {
+    lastImageRef.current.scrollIntoView({ behavior: "smooth" });
+    // setsnapScrollScreen("end");
+    console.log(snapScrollScreen);
+    setnxtBtnDisabled(true);
+    setprvBtnDisabled(false);
+  };
+  const scrollFirst = () => {
+    firstImageRef.current.scrollIntoView({ behavior: "smooth" });
+    setsnapScrollScreen("start");
+    console.log(snapScrollScreen);
+    setprvBtnDisabled(true);
+    setnxtBtnDisabled(false);
+  };
+  return (
+    <div
+      className="snap-scroll-box"
+      style={{
+        display: "flex",
+        width: "85vw",
+        marginLeft: "auto",
+        marginRight: "auto",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button
+        onClick={scrollFirst}
+        ref={prvBtnRef}
+        style={
+          prvBtnDisabled
+            ? { backgroundColor: "#c7c7cd", cursor: "auto" }
+            : { backgroundColor: "#939393", cursor: "pointer" }
+        }
+        className="scroll-Btn"
+      >
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <section className="snap-parent">
+        {images.map((image, index) => {
+          return (
+            <Link to={`/search=${index}`}>
+              <div
+                key={index}
+                ref={
+                  index == 0
+                    ? firstImageRef
+                    : index == images.length - 1
+                    ? lastImageRef
+                    : null
+                }
+                className="snap-child"
+              >
+                <img src={image} alt="..." />
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+      <button
+        onClick={scrollEnd}
+        ref={nxtBtnRef}
+        style={
+          nxtBtnDisabled
+            ? { backgroundColor: "#c7c7cd", cursor: "auto" }
+            : { backgroundColor: "#939393", cursor: "pointer" }
+        }
+        // disabled={nxtBtnDisabled}
+        // disabled={snapScrollScreen == "end" ? true : false}
+        className="scroll-Btn"
+      >
+        <FontAwesomeIcon icon={faChevronRight} />
+      </button>
+    </div>
+  );
+};
+
+export default Snapscroll;
